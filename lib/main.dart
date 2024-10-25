bbaa05b94908944606023cc388368dc015830da7
Kiash254 <codewithkiash@gmail.com>
2024-10-25 11:02:06 +0300

    added new data

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,49 @@
+import 'package:coffee_card/home.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('My Cofee  ID'),
-        backgroundColor: Colors.brown[700 ],
-        centerTitle: true,
-
-      ),
-      body:const Home(),
-    ),
+    home:Sandbox()
   ));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-     padding: const EdgeInsets.all(10),
-     margin: const EdgeInsets.only(left:50),
-      child: Text('Hello',
-      style: TextStyle(
-        fontSize: 18,
-        letterSpacing: 4,
-        decoration: TextDecoration.underline,
-        fontStyle: FontStyle.italic
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('SandBox'),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
       ),
-      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+     
+        children: [
+          Container(
+            width: 100,
+            color: Colors.brown[200],
+            padding: EdgeInsets.all(20),
+            child: const Text('How i like  Coffee.....',style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            ),),
+          ),
+           Container(
+            width: 200,
+            color: Colors.brown[300],
+            padding: EdgeInsets.all(30),
+            child: const Text('Coffee'),
+          ),
+      
+         
+        ],
+      )
     );
   }
 }
